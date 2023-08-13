f2d29cfc7c38665ceacb1e4cca70c27ebf4a0545
Emil <71653103+skemil@users.noreply.github.com>
2023-08-13 14:27:54 +0200

    Update products.11ty.js

--- cms/products.11ty.js
+++ cms/products.11ty.js
@@ -15,16 +15,6 @@ class ProductsData {
         if (!data.collections.product || !data.collections.product.length) {
             return JSON.stringify(products);
         }
-
-        if (window.netlifyIdentity) {
-          window.netlifyIdentity.on('init', user => {
-            if (!user) {
-              window.netlifyIdentity.on('login', () => {
-                document.location.href = '/admin/'
-              })
-            }
-          })
-        }
             
         for (let product of data.collections.product) {
             for (let variation of product.data.variations) {
